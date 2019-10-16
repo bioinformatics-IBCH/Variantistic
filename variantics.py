916ceeab57301bcf3e43b1dd2d40ac0a15c1ce44
Welekie <nedviga@rcpcm.org>
2019-10-16 13:51:20 +0300

    HGRESEARCH-28_fix_variantics_add_defs

--- variantics.py
+++ variantics.py
@@ -1,34 +1,46 @@
 
+from exitstatus import ExitStatus
+import sys
 import argparse
 import os
 import subprocess
 from os.path import basename
 
+def parse_args() -> argparse.Namespace:
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--vcf", type=str, action="store", help="input vcf data")
+	parser.add_argument("--output", type=str, action="store", help="output directory")
+	parser.add_argument("--type", type=str, action="store", help="type")
+	args = parser.parse_args()
+	return parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--vcf", type=str, action="store", help="input vcf data")
-parser.add_argument("--output", type=str, action="store", help="output directory")
-parser.add_argument("--type", type=str, action="store", help="type")
-args = parser.parse_args()
 
-if args.type == 'WES':
 
-	
-	
-	vcf = open(args.vcf)
-	vcf_first_line = vcf.readline()
-	vcf.close()
-	if vcf_first_line[0] == '#':
-		name_output = args.output + '/' + 'zapusk.txt'
-		file = open(name_output,'w')
-		file.write(args.vcf)
-		file.close()
-	else:
-		a = 'cp ' + args.vcf + ' ' + args.output + '/zapusk.txt'
+def main() -> ExitStatus:
+
+	args = parse_args()
+	if args.type == 'WES':
+
+		vcf = open(args.vcf)
+		vcf_first_line = vcf.readline()
+		vcf.close()
+		if vcf_first_line[0] == '#':
+			name_output = args.output + '/' + 'zapusk.txt'
+			file = open(name_output,'w')
+			file.write(args.vcf)
+			file.close()
+		else:
+			a = 'cp ' + args.vcf + ' ' + args.output + '/zapusk.txt'
+			subprocess.check_output(a, shell=True)
+
+		a = 'cp WriteListVcf.py '+ args.output + '/WriteListVcf.py;' + 'cp Snakefile ' + args.output + '/Snakefile; cd ' + args.output + '; snakemake --use-conda variant_statistics.tab.gz'
 		subprocess.check_output(a, shell=True)
 
-	a = 'cp script1.py '+ args.output + '/script1.py;' + 'cp Snakefile ' + args.output + '/Snakefile; cd ' + args.output + '; snakemake --use-conda variant_statistics.tab.gz'
-	subprocess.check_output(a, shell=True)
+	else:
+		print('Unsupported data type.')
 
-else:
-	print('Unsupported data type.')
+	return ExitStatus.success
+
+
+if __name__ == '__main__':
+    sys.exit(main())
