733d0c2ccd6ac32daa0d7e09f28918b5bdddbbce
Welekie <nedviga@rcpcm.org>
2019-11-21 13:00:03 +0300

    HGRESEARCH-33_include_lib_cyvcf2

--- lib.py
+++ lib.py
@@ -1,86 +1,92 @@
 from pysam import VariantFile
+import pysam
 import pandas 
 import numpy as np
 import matplotlib.pyplot as plt
+from cyvcf2 import VCF, Writer
+import os
+import subprocess
+import json
 
+csv = pandas.read_csv("testik.csv", sep=',')
+vcf_in = VCF('A.vcf')
+vcf_in.add_info_to_header(
+    {
+        'ID': 'VARIANTICS_HIST', 
+        'Description': '...',
+        'Type': 'String',
+        'Number': 1
+    }
+)
 
-def hist():
-
-    
-    file = open("A.vcf")
-    
-    f = open("header.vcf",'w')             # костыль, не знаю как по-другому
-    flag = 0
-    for i in file:
-        if i[0] == '#':
-            if i[2:6] == 'INFO' and flag == 0:
-                f.write('##INFO=<ID=DP_hist,Description="Raw read depth">')
-                flag = 1
-            f.write(i)
+vcf_out = Writer("A.processed.vcf", vcf_in)
+for rec in vcf_in:
+    ANM = []
+    ACM = []
+    DPM = []
+    DPM1 = rec.format('DP')
+    for i in range(len(DPM1)):
+        if DPM1[i][0] >= 0:
+            DPM.append(DPM1[i][0])
         else:
-            pass
-    file.close()
-    f.close() 
-    f = VariantFile("header.vcf")
-    vcf = VariantFile("A.vcf", header = f.header)
-    csv = pandas.read_csv("testik.csv", sep=',')
-#     vcf.header[58] = '##INFO=<ID=DP_hist,Description="Raw read depth">'
-    output_vcf = VariantFile('MergeWithHist.vcf', 'w', header = f.header)
-           
-  
-   
-    for rec in vcf.fetch():
-        for i in rec.info:
-        DPM = []
-        ACM = []
-        ANM = []
-        REC = rec.samples.keys()
+            DPM.append(0)
+    
+    GT = rec.format('GT')    # GT дает массив в байтах /x04/x04 пока что не знаю как это исправить
+    AC = 0
+    AN = 0
+    for sample in GT:
+        if sample == 1:
+            AC += 1
+            AN += 1
+        elif sample == 0:
+            AN += 1
+        ACM.append(AC)
+        ANM.append(AN)
+    
+    result = pandas.DataFrame({
+        "Age": csv.Age.tolist(),
+        "DiseaseId": csv.DiseaseId.tolist(),
+#         "Relativeness": csv.Relativeness,
+    }, index=csv["Sample name"])
+    result = np.array(result)  
+    bins=np.asarray([4,4])
+    try:
+        A = np.histogramdd(result,bins=bins,weights=DPM)
+    except:
+#         print('Don't find DP in format)
+        A = [np.array([]),[]]
 
-        for i in REC:
-            AC = 0
-            AN = 0
-            
-            if rec.samples[i].get("DP", None) is not None:
-                DPM.append(rec.samples[i]["DP"])
-            else:
-                DPM.append(0)
-            GT = rec.samples[i].get("GT",'./.')
-
-            for j in GT:
-
-                if j == 1:
-                    AC += 1
-                    AN += 1
-                elif j == 0:
-                    AN += 1
-            ACM.append(AC)
-            ANM.append(AN)
-        result = pandas.DataFrame({
-            "Age": csv.Age.tolist(),
-            "DiseaseId": csv.DiseaseId.tolist(),
-            #         "Relativeness": csv.Relativeness,
-            # "DP": DPM,
-            # "AN": ANM,
-            #         "AC": ACM
-        }, index=csv["Sample name"])
-        result = np.array(result)
-
-        bins = np.asarray([4, 4])
-        try:
-            A = np.histogramdd(result, bins=bins, weights=DPM)
-        except:
-            print("Don't find DP in format",DPM)
-            A = []
-
-        try:
-            B = np.histogramdd(result, bins=bins, weights=ANM)
-            C = np.histogramdd(result, bins=bins, weights=ACM)
-        except:
-#             print("Don't find GT in format ")
-            B = []
-            C = []
-        rec.info = rec.info + ';' + str(A)     # костыль, не знаю как нормально добавить гистограмы 
+    try:
+        B = np.histogramdd(result,bins=bins,weights=ANM)
+        C = np.histogramdd(result,bins=bins,weights=ACM)
+    except:
+#       print('Don't find GT in format)
+        B = [[],[]]
+        C = [[],[]]
         
-        output_vcf.write(rec)
-        
-    output_vcf.close()
+    list = [A,B,C]
+    for i in list:
+        for j in range(len(i[1])):
+            i[1][j] = i[1][j].tolist()
+
+    VARIANTICS_HIST = json.dumps(
+        {
+            'DP_HIST': {
+                'hist': A[0].tolist(),
+                'edges': A[1]
+             },
+             'AN_HIST': {
+                'hist': B[0].tolist(),
+                'edges': B[1]
+             },
+             'AC_HIST': {
+                'hist': C[0].tolist(),
+                'edges': C[1]
+             }
+        }
+    )
+    rec.INFO["VARIANTICS_HIST"] = VARIANTICS_HIST
+    vcf_out.write_record(rec)
+vcf_in.close()
+vcf_out.close() 
+
