55dec6ea5e4f0d900bdc485b2b4266110f7f7f28
Welekie <nedviga@rcpcm.org>
2019-12-18 11:53:37 +0300

    HGRESEARCH-33_remake_valid_make_all_points

--- src/hist/Const.py
+++ src/hist/Const.py
@@ -1,4 +1,4 @@
 import numpy as np
-Bins = np.asarray([4,1,4,4,2])
+Bins = np.asarray([4,2,4,4,3])
 categorial_metadata = ("Sex", "Phenotype")
 Sample_name = 'Sample name'
--- src/hist/valid.py
+++ src/hist/valid.py
@@ -3,14 +3,39 @@ from hist.Const import Bins
 
 
 def validation(csv):
-    b = 1
-    for i in Bins:
-        b = b * i
-    if len(csv.index) < b:
-        return False
-    check = [1 for i in csv.index]
-    Check = makeHist(csv, check)
-    flag = proverka(Check[0])
+    checkWeight = [1]*len(csv)
+    CheckHist = makeHist(csv, checkWeight)
+	TrueSize = []
+	Dart = CheckHist[0]
+	TrueCheck = [[0] * len(Dart), [0] * len(Dart[0]), [0] * len(Dart[0][0]), [0] * len(Dart[0][0][0]), [0] * len(Dart[0][0][0][0])]
+	for i1 in range(len(Dart)):
+		CheckNow1 = []
+		for i2 in range(len(Dart[i1])):
+			CheckNow2 = []
+			for i3 in range(len(Dart[i1][i2])):
+				CheckNow3 = []
+				for i4 in range(len(Dart[i1][i2][i3])):
+					TrueCheck[3][i4] += sum(Dart[i1][i2][i3][i4])
+					CheckNow3.append(sum(Dart[i1][i2][i3][i4]))
+					for i5 in range(len(Dart[i1][i2][i3][i4])):
+						TrueCheck[4][i5] += Dart[i1][i2][i3][i4][i5]
+				TrueCheck[2][i3] += sum(CheckNow3)
+				CheckNow2.append(sum(CheckNow3))
+			TrueCheck[1][i2] += sum(CheckNow2)
+			CheckNow1.append(sum(CheckNow2))
+		TrueCheck[0][i1] += sum(CheckNow1)
+	for i in TrueCheck:
+		flag = 0
+		for j in i:
+			if j > 0:
+				flag += 1
+		TrueSize.append(flag)
+	b = 1
+	for i in TrueSize:
+		b = b * i
+	if len(csv.index) < b:
+		return False
+    flag = proverka(CheckHist[0])
     if flag > 0:
         return False
     else:
@@ -19,8 +44,9 @@ def validation(csv):
 
 
 def proverka(check):
+	""" Рекурсивно проверяет количество образцов в корзине. Возвращает количество ячеек в которых всего 1 элемент"""
     flag =0
-    if str(type(check[0])) == "<class 'numpy.ndarray'>":
+    if  not isinstance(check[0],(list, tuple)):
         for i in check:
             flag = flag + proverka(i)
     else:
